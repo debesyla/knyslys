5dc34da3d5af8176e476dfd1f189f26f670f5168
Danielius Goriunovas <lopasada@gmail.com>
2025-12-20 01:47:08 +0200

    Adjust badge update thresholds for improved performance

--- background.js
+++ background.js
@@ -30,9 +30,7 @@ ext.runtime.onMessage.addListener((msg, sender, sendResponse) => {
                 cachedDomainCount += addedCount;
 
                 // Only update badge if count changed significantly
-                // @TEMPORARY: for testing purposes, reduce thresholds
-                // if (addedCount >= 5 || cachedDomainCount % 10 === 0) {
-                if (addedCount >= 3 || cachedDomainCount % 5 === 0) {
+                if (addedCount >= 5 || cachedDomainCount % 10 === 0) {
                     clearTimeout(updateBadgeTimer);
                     updateBadgeTimer = setTimeout(() => {
                         const count = cachedDomainCount;
